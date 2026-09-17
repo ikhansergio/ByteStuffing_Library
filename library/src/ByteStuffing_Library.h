4a389e7793774ab2e2470ec3dc4dc8f396856c49
Sergio Batu <ikhan.sergio@gmail.com>
2026-09-17 11:45:34 +0300

    Update ByteStuffing_Library.h

--- library/src/ByteStuffing_Library.h
+++ library/src/ByteStuffing_Library.h
@@ -1,1 +1,22 @@
+#ifndef BYTESTUFFING_LIBRARY_H
+#define BYTESTUFFING_LIBRARY_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+#define MAX_PackSize 8192
+
+#define FrameDelimiter          0x7E
+#define EscapeSymbol            0x7D
+#define EscapeMask0             0x5E
+#define EscapeMask1             0x5D
+
+unsigned char ByteStuffingDecoder(unsigned char *DataIn, unsigned char *DataOut);
+unsigned char ByteStuffingEncoder(unsigned char *DataIn, unsigned char *DataOut_x2);
+#ifdef __cplusplus
+}
+#endif
+
+#endif // BYTESTUFFING_LIBRARY_H
